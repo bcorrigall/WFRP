4baa77aa588a50c2f35e3ff527677ad4374cdb3f
bcorrigall <byroncorrigall@gmail.com>
2021-11-19 11:59:17 -0500

    Added a class selector based on race

--- chargen.py
+++ chargen.py
@@ -1,24 +1,32 @@
 import random
 
-def raceselect():
+def race_select():
     """returns a randomly selected race"""
-    rnum = random.randint(1,100)
-    if rnum == 100:
+    race_num = random.randint(1,100)
+    if race_num == 100:
         race = 'Wood Elf'
-    elif rnum == 99:
+    elif race_num == 99:
         race = 'High Elf'
-    elif rnum >= 95:
+    elif race_num >= 95:
         race = 'Dwarf'
-    elif rnum >= 91:
+    elif race_num >= 91:
         race = 'Halfling'
-    elif rnum <= 90: 
+    elif race_num <= 90: 
         race = 'Human'
-    print(rnum)
-    return race
+    return race, race_num
 
-def class_select():
-    """"""
+race, racenum = race_select()
 
+def class_select(race):
+    """returns a randomly selected class depending on race"""
+    class_list = []
+    class_chance = []
+    with open(race+' Classes.txt', 'r') as file:
+        for line in file:
+            class_line = line.strip('\n').split('_')
+            class_chance.append(int(class_line[0]))
+            class_list.append(class_line[1])
+    player_class = random.choices(class_list, weights = class_chance)
+    return player_class[0]
 
-
-print(raceselect())
+print(class_select('Wood Elf'))
