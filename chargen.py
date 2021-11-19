13143c2de8c8b9439e7ce02fef18594b4dae7721
bcorrigall <byroncorrigall@gmail.com>
2021-11-18 22:41:24 -0500

    Update chargen.py

--- chargen.py
+++ chargen.py
@@ -1,17 +1,24 @@
 import random
 
 def raceselect():
-    rnum = random.random()
-    if rnum >= 0.99:
+    """returns a randomly selected race"""
+    rnum = random.randint(1,100)
+    if rnum == 100:
         race = 'Wood Elf'
-    elif rnum >= 0.98:
+    elif rnum == 99:
         race = 'High Elf'
-    elif rnum >= 0.94:
+    elif rnum >= 95:
         race = 'Dwarf'
-    elif rnum >= 0.9:
+    elif rnum >= 91:
         race = 'Halfling'
-    elif rnum >= 0: 
+    elif rnum <= 90: 
         race = 'Human'
+    print(rnum)
     return race
+
+def class_select():
+    """"""
+
+
 
 print(raceselect())
